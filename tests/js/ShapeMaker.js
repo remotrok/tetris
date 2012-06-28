4088efa2cc41f61b6b5aca176fad0228e89f64be
Silvio René <silvio.rene@gmail.com>
2012-06-28 11:37:17 +0200

    added ShapeMaker tests

--- tests/js/ShapeMaker.js
+++ tests/js/ShapeMaker.js
@@ -2,7 +2,7 @@
 
 suite('ShapeMaker');
 
-test('ShapeConstructorMakesCopies', function () {
+test('Shape Constructor Makes Copies', function () {
 	var shape, shape2;
 
 	shape = ShapeMaker.make(ShapeMaker.tetramins[0]);
@@ -17,14 +17,27 @@ test('ShapeConstructorMakesCopies', function () {
 	expect(shape.coords[0][0]).to.not.equal(shape2.coords[0][0]);
 });
 
-test('koko', function () {
-	expect(1).to.be.ok();
+test('A Shape have an id(string)', function () {
+	var shape = ShapeMaker.getRandomShape();
+	expect(shape.id).be.an('string');
 });
 
-test('popo', function () {
-	expect(1).to.not.be.ok();
+test('A Shape have a color(string)', function () {
+	var shape = ShapeMaker.getRandomShape();
+	expect(shape.color).be.an('string');
 });
 
-test('koko', function () {
-	expect(1).to.be.ok();
+test('A Shape have coords(Array)', function () {
+	var shape = ShapeMaker.getRandomShape();
+	expect(shape.coords).be.an(Array);
+});
+
+test('A Shape id is one of C, Z, L, LI, B, S or T', function () {
+	var shape = ShapeMaker.getRandomShape();
+	expect(['S', 'Z', 'L', 'LI', 'B', 'S', 'T']).to.contain(shape.id);
+});
+
+test('A Shape is formed by 4 squares', function () {
+	var shape = ShapeMaker.getRandomShape();
+	expect(shape.coords.length).to.be(4);
 });
